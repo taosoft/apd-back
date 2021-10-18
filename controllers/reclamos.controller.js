e9dc78fbca0e0633cea9a885a72191fc5ddd3d90
Nico-Santos <Legendary_32>
2021-10-18 14:20:29 -0300

    GetReclamos and GetReclamos done

--- controllers/reclamos.controller.js
+++ controllers/reclamos.controller.js
@@ -4,11 +4,31 @@ const ReclamoService = require("../services/reclamos.service");
 _this = this;
 
 exports.getReclamo = async (req, res, next) => {
-
+    try {
+        const reclamo = await ReclamoService.getReclamo(req.params.id);
+        return res.status(200).json({
+            status: 200,
+            data: reclamo,
+            message: "Successfully Reclamo Received",
+        });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 };
 
 exports.getReclamos = async (req, res, next) => {
+    try {
+        const pagination = req.query.quantity ? req.query.quantity : 0;
+        const reclamos = await ReclamoService.getReclamos(pagination);
 
+        return res.status(200).json({
+            status: 200,
+            data: reclamos,
+            message: "Successfully Reclamos Received",
+        });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 };
 
 exports.createReclamo = async (req, res, next) => {
--- services/reclamos.service.js
+++ services/reclamos.service.js
@@ -1,24 +1,35 @@
-
+const ReclamoModel = require('../models/reclamos.model');
 
 // Saving the context of this module inside the _the variable
 _this = this;
 
-exports.getReclamo = async (req, res, next) => {
+exports.getReclamo = async (reclamoId) => {
+    try {
+        return await ReclamoModel.findByPk(reclamoId);
+    } catch (error) {
+        throw Error("Error while searching Reclamo");
+    }
+};
+
+exports.getReclamos = async (quantity) => {
+    try {
+        return await ReclamoModel.findAll({ 
+            limit: quantity,
+            order: ['idReclamo','documento']
+        });
+    } catch (error) {
+        throw Error("Error while searching Reclamos");
+    }
+};
+
+exports.createReclamo = async () => {
 
 };
 
-exports.getReclamos = async (req, res, next) => {
-
-};
-
-exports.createReclamo = async (req, res, next) => {
-
-};
-
-exports.unificarReclamo = async (req, res, next) => {
+exports.unificarReclamo = async () => {
     
 };
 
-exports.updateReclamo = async (req, res, next) => {
+exports.updateReclamo = async () => {
 
 };
