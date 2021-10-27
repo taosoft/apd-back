77d66dfbe2c44063d9afdbd0447abd0e90e8e0e3
Thomas B <braithwaite.th@gmail.com>
2021-10-26 23:39:06 -0300

    No vamos a usar este endpoint

--- routes/reclamos.js
+++ routes/reclamos.js
@@ -5,7 +5,7 @@ const ReclamoController = require("../controllers/reclamos.controller");
 router.get("/", ReclamoController.getReclamos);
 router.get("/:id", ReclamoController.getReclamo);
 router.post("/", ReclamoController.createReclamo);
-router.post("/unificar", ReclamoController.unificarReclamo);
+// router.post("/unificar", ReclamoController.unificarReclamo); //Lo hace el municipio
 router.patch("/:id", ReclamoController.updateReclamo);
 
 // Export the Router
