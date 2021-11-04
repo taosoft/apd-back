8034e5b1d971a7c4f64d5bb8d4ff4c055c06ba1a
Nico-Santos <santos.m.nicolas@gmail.com>
2021-11-04 18:22:49 -0300

    Get comercio endponit

--- controllers/comercios.controller.js
+++ controllers/comercios.controller.js
@@ -4,11 +4,31 @@ const ComercioService = require("../services/comercios.service");
 _this = this;
 
 exports.getComercio = async (req, res, next) => {
-
+    try {
+        const reclamo = await ComercioService.getComercio(req.params.id);
+        return res.status(200).json({
+            status: 200,
+            data: reclamo,
+            message: "Successfully Comercio Received",
+        });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 };
 
 exports.getComercios = async (req, res, next) => {
+    try {
+        const pagination = req.query.quantity ? parseInt(req.query.quantity) : 10;
+        const denuncias = await ComercioService.getComercios(pagination);
 
+        return res.status(200).json({
+            status: 200,
+            data: denuncias,
+            message: "Successfully Comercios Received",
+        });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 };
 
 exports.createComercio = async (req, res, next) => {
--- services/comercios.service.js
+++ services/comercios.service.js
@@ -1,16 +1,27 @@
-
+const ComercioModel = require("../models/comercios.model");
 
 // Saving the context of this module inside the _the variable
 _this = this;
 
-exports.getComercio = async (req, res, next) => {
-
+exports.getComercio = async (comercioId) => {
+    try {
+        return await ComercioModel.findByPk(comercioId);
+    } catch (error) {
+        throw Error("Error while searching Comercio | ", error);
+    }
 };
 
-exports.getComercios = async (req, res, next) => {
-
+exports.getComercios = async (pagination) => {
+    try {
+        return await ComercioModel.findAll({
+            limit: quantity,
+            order: ['idComercio', 'documento']
+        });
+    } catch (error) {
+        throw Error("Error while searching Comercios | ", error);
+    }
 };
 
-exports.createComercio = async (req, res, next) => {
+exports.createComercio = async () => {
 
 };
