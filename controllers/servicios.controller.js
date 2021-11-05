748721b5039e1dc046063046e85cee334a0ac5ce
Nico-Santos <santos.m.nicolas@gmail.com>
2021-11-05 12:36:03 -0300

    Flow de servicio creado

--- controllers/servicios.controller.js
+++ controllers/servicios.controller.js
@@ -4,13 +4,60 @@ const ServicioService = require("../services/servicios.service");
 _this = this;
 
 exports.getServicio = async (req, res, next) => {
-
+    try {
+        const sitio = await ServicioService.getServicio(req.params.id);
+        return res.status(200).json({
+            status: 200,
+            data: sitio,
+            message: "Serivicio encontrado exitósamente",
+        });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 };
 
 exports.getServicios = async (req, res, next) => {
+    try {
+        const pagination = req.query.quantity ? parseInt(req.query.quantity) : 10;
+        const sitios = await ServicioModel.getServicios(pagination);
 
+        return res.status(200).json({
+            status: 200,
+            data: sitios,
+            message: "Serivicios encontrados exitósamente",
+        });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 };
 
 exports.createServicio = async (req, res, next) => {
+    try {
+        if(await ServicioService.existeServicio(req.body.nombreServicio)) {
+            throw new Error(`El servicio ${req.body.nombreServicio} ya existe`);
+        }
 
+        const datosServicio = {
+            idRubro: +req.body.idSitio,
+            nombreServicio: req.body.nombreServicio,
+            nombrePersona: req.body.nombrePersona,
+            direccion: req.body.direccion,
+            telefono: req.body.telefono,
+            email: req.body.email,
+            horario: req.body.horario,
+            descripcion: req.body.descripcion ? req.body.descripcion : '',
+            archivosURL: req.body.archivosURL ? req.body.archivosURL : ''
+        }
+
+        const servicioCreated = await ServicioService.createServicio(datosServicio);
+
+        return res.status(200).json({
+            status: 200,
+            data: servicioCreated,
+            message: "Serivicio creado exitósamente",
+        });
+    } catch (e) {
+        console.log(e)
+        return res.status(400).json({ status: 400, message: e.message });
+    }
 };
--- services/servicios.service.js
+++ services/servicios.service.js
@@ -1,16 +1,42 @@
-
+const ServicioModel = require('../models/servicios.model');
 
 // Saving the context of this module inside the _the variable
 _this = this;
 
-exports.getServicio = async (req, res, next) => {
-
+exports.getServicio = async (servicioId) => {
+    try {
+        return await ServicioModel.findByPk(servicioId);
+    } catch (error) {
+        throw Error(`Error al buscar el Servicio ${servicioId} | `, error);
+    }
 };
 
-exports.getServicios = async (req, res, next) => {
-
+exports.getServicios = async (pagination) => {
+    try {
+        return await ServicioModel.findAll({
+            limit: pagination,
+            order: ['idServicio', 'nombreServicio']
+        });
+    } catch (error) {
+        throw Error(`Error al buscar todos los servicios | `, error);
+    }
 };
 
-exports.createServicio = async (req, res, next) => {
+exports.createServicio = async (newServicio) => {
+    try {
+        return await ServicioModel.create(newServicio);
+    } catch (error) {
+        throw Error(`Error al crear el servicio ${newServicio.nombreServicio} | `, error);
+    }
+};
 
+exports.existeServicio = async (nombreDelServicio) => {
+    try {
+        const existeServicio = await ServicioModel.findOne({ where: { nombreServicio: nombreDelServicio } });
+
+        if (existeServicio === null) return false;
+        else return true;
+    } catch (error) {
+        throw Error(`Error al buscar si el servicio ${nombreDelServicio} ya existe | `, error)
+    }
 };
