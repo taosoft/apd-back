cd2e8dcc6097dc5ce0b3d38067af7c2f93926179
Nico-Santos <santos.m.nicolas@gmail.com>
2021-11-09 21:39:19 -0300

    WIP

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 require('moment/locale/es');
 
 // Verificacion
-// console.log(moment.locale());
+console.log("Verificación de tiempo, locale: ", moment.locale());
 
 // Database connection
 const mysql_connection = require('./database');
