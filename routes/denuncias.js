e9daa3c208f5e4dcc985ec5d7a145ba1a828fc59
Matías <31491708+taosoft@users.noreply.github.com>
2021-11-11 19:31:57 -0300

    Develop (#10)
    * Verifica que existe vecino al momento de crear reclamo
    
    * Fecha y Hora arreglado
    
    * Libreria quedó colgada
    
    * WIP
    
    * Creacion users hecha
    
    * log in cambiado a entrar con documento en vez de email
    
    * WIP
    
    * finish update estado reclamo
    
    * WIP
    
    * WIP
    
    * Quito ; de bitacora cuando creo reclamo
    
    * Denuncia Flow
    
    * Get comercio endponit
    
    * Comercio endpoint
    
    * Create Denuncia
    
    * Nombre metodo Create Denuncia fixed
    
    * Sitios Flow
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Denuncias done
    
    * Declaracion de modelos arregaldos. unique:true borrado
    
    * Flow de servicio creado
    
    * Route servicio descomentado de APP
    
    * Nombre de import arreglado
    
    * Endpoints servicios funcionando
    
    * Endpoint comercio funcionando
    
    * Descomentado el route de comercio
    
    * Movimiento Reclamo
    
    * Endpoint Movimientos de Reclamo creado
    
    * Endpoint movimiento Denuncias hecho
    
    * Endpoint movimiento reclamos hecho
    
    * Cuando crea el reclamo popula tabla movimientosReclamo
    
    * Populacion de tabla movienteDenuncia al crear una denuncia
    
    * Tabla movimientosDenuncias populada
    
    * Atributo inspector agregado al modelo, controller y service
    
    * Cambio de URL
    
    * corregido atributo de busqueda en update
    
    * Denuncia Endpoint terminado
    
    * Listo
    
    * Endpoint rubro terminado
    
    * Endpoint desperfecto hecho
    
    * Rutas para endpoint rubro y desperfecto
    
    * Update pass or doc or both
    
    * Return true or false when updating user
    
    * Token asociado
    
    * Token asociado a llamadas para consumir en controller
    
    * endpoint para buscar un desperfecto
    
    * endpoint para traer denuncias de un usuario
    
    * Moment default
    
    * WIP
    
    * WIP 2
    
    * New endpoint with join
    
    * Denuncia detalle join done
    
    * Endpoint servicio detalle done
    
    * Toma doc del token para denuncias
    
    * WIP
    
    Co-authored-by: Nico-Santos <santos.m.nicolas@gmail.com>
    Co-authored-by: Thomas B <braithwaite.th@gmail.com>
    Co-authored-by: Matias Grieben <mgrieben@uade.edu.ar>
    Co-authored-by: Nico-Santos <Legendary_32>

--- routes/denuncias.js
+++ routes/denuncias.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const DenunciaController = require("../controllers/denuncias.controller");
 const Authorization = require("../authorization/authorization");
 
-router.get("/usuario/:id", Authorization, DenunciaController.getDenuncias);
+router.get("/usuario", Authorization, DenunciaController.getDenuncias);
 router.get("/:id", Authorization, DenunciaController.getDenuncia);
 router.get("/detalle/:id", Authorization, DenunciaController.getDenunciaDetalle);
 router.post("/", Authorization, DenunciaController.createDenuncia);
