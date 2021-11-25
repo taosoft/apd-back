dbf08d1cc3203e24f4560a60c0437b2b5f0d969b
Matias Grieben <mgrieben@uade.edu.ar>
2021-11-25 00:34:10 -0300

    WIP

--- controllers/reclamos.controller.js
+++ controllers/reclamos.controller.js
@@ -61,7 +61,7 @@ exports.createReclamo = async (req, res, next) => {
         const datosReclamo = {
             documento: req.documento,
             idSitio: +req.body.idSitio,
-            idRubro: +req.id.idRubro,
+            idRubro: +req.idRubro,
             idDesperfecto: +req.body.idDesperfecto,
             descripcion: req.body.descripcion ? req.body.descripcion : '',
             estado: "Iniciado",
